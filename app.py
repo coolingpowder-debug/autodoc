ff4849e56ab68ff4c2a79a1340aa0a88758470ef
coolingpowder-debug <coolingpowder@gmail.com>
2026-09-22 11:23:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -287,6 +287,6 @@ st.markdown("---")
 st.markdown("🛠️ **สร้างและพัฒนาโดย:** Admin Mine")
 
 with st.expander("ช่องทางติดต่อ: cool***@gmail.com (คลิกเพื่อดูอีเมลทั้งหมด)"):
-    full_email = "coolingpowder@gmail"
+    full_email = "coolingpowder@gmail.com"
     st.markdown(f"📧 **อีเมลติดต่อ:** `{full_email}`")
     st.markdown(f"🔗 [คลิกที่นี่เพื่อส่งอีเมลหาผู้พัฒนา](mailto:{full_email})")
